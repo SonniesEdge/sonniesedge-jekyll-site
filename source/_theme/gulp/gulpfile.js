937e9ccb66991ac4fccf7d7a7a0db65c339ba8ae
Charlie Owen <charlie@sonniesedge.co.uk>
2015-05-11 21:26:26 +0100

    Integrate theme

--- source/_theme/gulp/gulpfile.js
+++ source/_theme/gulp/gulpfile.js
@@ -4,12 +4,12 @@ var watch = require('gulp-watch');
 var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('default', function() {
-  return gulp.src('../design/stylesheets/source/main.scss')
+  return gulp.src('../stylesheets/main.scss')
     // .pipe(watch('sass/*.scss'))
     .pipe(sass())
     .pipe(autoprefixer({
                 browsers: ['last 2 versions'],
                 cascade: false
             }))
-    .pipe(gulp.dest('../design/stylesheets/build'));
+    .pipe(gulp.dest('../../../_build/css'));
 });
