c3cdb68bf76cd7972b1bb5e12dc950f5716ba697
Charlie Owen <charlie.owen@bbc.co.uk>
2015-05-12 09:59:07 +0100

    Update gulfile to run

--- source/_theme/gulp/gulpfile.js
+++ source/_theme/gulp/gulpfile.js
@@ -3,13 +3,25 @@ var sass = require('gulp-sass');
 var watch = require('gulp-watch');
 var autoprefixer = require('gulp-autoprefixer');
 
-gulp.task('default', function() {
-  return gulp.src('../design/stylesheets/source/main.scss')
+gulp.task('jekyll', function (gulpCallBack){
+    process.chdir('../../../');
+    var spawn = require('child_process').spawn;
+    var jekyll = spawn('jekyll', ['build', '--config', '_config.yml'], {stdio: 'inherit'});
+
+    jekyll.on('exit', function(code) {
+        gulpCallBack(code === 0 ? null : 'ERROR: Jekyll process exited with code: '+code);
+    });
+});
+
+gulp.task('default', ['jekyll'],function() {
+  return gulp.src('../stylesheets/main.scss')
     // .pipe(watch('sass/*.scss'))
     .pipe(sass())
     .pipe(autoprefixer({
                 browsers: ['last 2 versions'],
                 cascade: false
             }))
-    .pipe(gulp.dest('../design/stylesheets/build'));
+    .pipe(gulp.dest('../../../_build/css'));
 });
+
+
